243f95e11f5ae61ca5334c3d1f5b89127de728ce
Gaurav Pandvia <gaurav.pandvia@minjar.com>
2016-09-26 04:11:45 +0530

    First commit in wedding page.

---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+// document ready
+$(function() {
+    // Photos Slideshot
+    // Groomsmen Slideshow
+    $(".slideshow").owlCarousel({
+
+        navigation : true, // Show next and prev buttons
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true,
+        paginationNumbers : true,
+        responsive : true
+
+        // "singleItem:true" is a shortcut for:
+        // items : 1,
+        // itemsDesktop : false,
+        // itemsDesktopSmall : false,
+        // itemsTablet: false,
+        // itemsMobile : false
+
+    });
+});
